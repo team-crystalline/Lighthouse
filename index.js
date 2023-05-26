c61967b7f54cac73d0e39ebecd4f568f4194656f
Dee Liehr <dannyliehr@gmail.com>
2023-05-26 19:40:01 -0400

    Fixed sitemap

--- index.js
+++ index.js
@@ -1794,12 +1794,8 @@ var sysArr;
 		res.send(`
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-
+      xmlns="http://www.writelighthouse.com"
+      xsi:schemaLocation="http://www.writelighthouse.com/sitemap.xml">
 
 <url>
   <loc>https://www.writelighthouse.com/</loc>

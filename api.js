771d73539a24946c1bb607f90451f71dd5f85072
DannyLiehr <dannyisyelling@gmail.com>
2023-12-01 22:30:10 -0500

    Testing adding members endpoint

--- api.js
+++ api.js
@@ -76,7 +76,7 @@ router.get("/test", async function(req, res){
 
 // Grab user's members by user ID and token. If the token is not provided, reject them. The token should not be in the URL
 router.get('/members/:id', async function (req, res){
-    if (apiEyesOnly(req)){
+    // if (apiEyesOnly(req)){
       let userCheck= await db.query(client, "SELECT users.id, tokens.* FROM users INNER JOIN tokens ON users.id= tokens.u_id WHERE users.id=$1;", [req.params.id], res, req);
       let matched= false;
       // Using a for loop bc I can easily break out of it.
@@ -130,9 +130,9 @@ router.get('/members/:id', async function (req, res){
       } else {
         return res.status(401).send("Not Authorised")
       }
-    } else {
-      return res.status(404).render('pages/404',{ session: req.session, code:"Not Found", cookies:req.cookies });
-    }
+    // } else {
+    //   return res.status(404).render('pages/404',{ session: req.session, code:"Not Found", cookies:req.cookies });
+    // }
   });
 // Grab user's systems by user ID and token. If the token is not provided, reject them. The token should not be in the URL
 router.get('/systems/:id', async function (req, res){
